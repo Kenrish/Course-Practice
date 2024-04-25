d45cf1a843f3a18e16180a110ff17c79ec32aa18
Kenrish <rishabhkengeri08@gmail.com>
2024-04-25 14:14:51 +0530

    Rename min_absolute to min_gb, use perameter names

--- allchecks.py
+++ allchecks.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
 
 import shutil
+import os
 import sys
 
 def check_reboot():
     """Return True if the computer has a pending reboot."""
     return os.path.exists("/run/reboot-required")
 
-def check_disk_full(disk, min_absolute, min_percent):
+def check_disk_full(disk, min_gb, min_percent):
     """Return True if there isn't enough disk space, False otherwise."""
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
     # Calculate how many free gigabytes
-    gigabytes_free = du.free < min_percent or gigabytes_free < main_absolute:
+    gigabytes_free = du.free / 2**30
+    if percent_free < min_percent or gigabytes_free < min_gb:
         return True
     return False
 
@@ -21,8 +23,11 @@ def main():
     if check_reboot():
         print("Pending Reboot.")
         sys.exit(1)
-    if check_disk_full("/", 2, 10):
+    if check_disk_full(disk="/", min_gb=2, min_percent=10):
         print("Disk Full!")
-        sys.exit(0)
+        sys.exit(1)
+
+    print("Everything ok.")
+    sys.exit(0)
 
 main()
